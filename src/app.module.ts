2fff04d7a98dc81bc2e47fca80d1cf1b928472b2
Larisa Ponce <nataly.linarca@gmail.com>
2025-05-15 11:15:13 -0600

    a saber si funciona

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EventosController } from './eventos/eventos.controller';
+import { EventosService } from './eventos/eventos.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, EventosController],
+  providers: [AppService, EventosService],
 })
 export class AppModule {}
