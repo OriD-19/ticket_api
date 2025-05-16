090b327117b07946082cb49df5eaa449dc2efcfe
Fer Fuentes <fernandocastillo.8@hotmail.com>
2025-05-15 18:28:27 -0600

    fix: removed food controller from imports array in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TicketsModule } from './tickets/tickets.module';
-import { FoodStatusController } from './food_status/food_status.controller';
 import { PrintTicketModule } from './Print_ticket/Print_ticket.module';
 import { VideogamesModule } from './videogames/videogames.module';
 import { BooksModule } from './books/books.module';
 import { EventosModule } from './eventos/eventos.module';
+import { FoodOrderModule } from './food_status/food_order.module';
 
 @Module({
-  imports: [TicketsModule, FoodStatusController, PrintTicketModule, BooksModule, VideogamesModule, EventosModule],
+  imports: [TicketsModule, FoodOrderModule, PrintTicketModule, BooksModule, VideogamesModule, EventosModule],
   controllers: [AppController],
   providers: [AppService],
 })
